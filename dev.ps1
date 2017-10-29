7a8b41de0ec4d4455ace3f66111fd01a1b1cda7c
Yinyin Qian <yyqian@live.com>
2017-10-29 10:12:32 +0800

    updatre

--- dev.ps1
+++ dev.ps1
@@ -1,3 +1,3 @@
 docker build -t yyqian/ubuntu .
 docker rmi $(docker images -f "dangling=true" -q)
-#docker run -it --rm -v d:/:/mnt/d yyqian/ubuntu bash
+docker run -it --rm -v d:/:/mnt/d yyqian/ubuntu bash
